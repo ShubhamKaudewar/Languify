02699ea1a7574ccd55ed40cae25ece84954bc113
ShubhamKaudewar <kaudewar@gmail.com>
2020-05-29 03:14:13 +0530

    VideoRecorderCreated

--- videoRecorder/upload-video.php
+++ videoRecorder/upload-video.php
@@ -1,11 +1,15 @@
 <?php
  
 if(isset($_FILES["video"])){
+
+    // The Below Code is not working for unique video
+    // $fileName = "file".date('m-d-Y-His A e').".webm";
+
     // Define a name for the file
-    $fileName = "myvideo.webm";
+    $fileName = "RecordedVideo1.webm";
 
     // In this case the current directory of the PHP script
-    $uploadDirectory = './'. $fileName;
+    $uploadDirectory = './records/'.$fileName;
     
     // Move the file to your server
     if (!move_uploaded_file($_FILES["video"]["tmp_name"], $uploadDirectory)) {
