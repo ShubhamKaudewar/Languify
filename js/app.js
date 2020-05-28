e579c92b4fde363fea1aaef13bfde98befde9b1e
ShubhamKaudewar <kaudewar@gmail.com>
2020-05-28 15:29:20 +0530

    First commit

---
+++ js/app.js
@@ -0,0 +1,124 @@
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var recorder; 						//WebAudioRecorder object
+var input; 							//MediaStreamAudioSourceNode  we'll be recording
+var encodingType; 					//holds selected encoding for resulting audio (file)
+var encodeAfterRecord = true;       // when to encode
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext; //new audio context to help us record
+
+// var encodingTypeSelect = document.getElementById("encodingTypeSelect");
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+
+function startRecording() {
+	console.log("startRecording() called");
+
+	/*
+		Simple constraints object, for more advanced features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
+    var constraints = { audio: true, video:false }
+
+    /*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		// __log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
+
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+
+		*/
+		audioContext = new AudioContext();
+
+		//assign to gumStream for later use
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+		
+		//stop the input from playing back through the speakers
+		//input.connect(audioContext.destination)
+
+		//get the encoding 
+		// encodingType = encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value;
+		encodingType = "wav";
+		
+		//disable the encoding selector
+		// encodingTypeSelect.disabled = true;
+
+		recorder = new WebAudioRecorder(input, {
+		  workerDir: "js/", // must end with slash
+		  encoding: encodingType,
+		  numChannels:2, //2 is the default, mp3 encoding supports only 2
+		});
+
+		recorder.onComplete = function(recorder, blob) {
+			createDownloadLink(blob,recorder.encoding);
+			// encodingTypeSelect.disabled = false;
+		}
+
+		recorder.setOptions({
+		  timeLimit:120,
+		  encodeAfterRecord:encodeAfterRecord,
+	      ogg: {quality: 0.5},
+	      mp3: {bitRate: 160}
+	    });
+
+		//start the recording process
+		recorder.startRecording();
+
+	}).catch(function(err) {
+	  	//enable the record button if getUSerMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+
+	});
+
+	//disable the record button
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopRecording() {
+	console.log("stopRecording() called");
+	
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//disable the stop button
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	
+	//tell the recorder to finish the recording (stop recording + encode the recorded audio)
+	recorder.finishRecording();
+}
+
+function createDownloadLink(blob,encoding) {
+	var url = URL.createObjectURL(blob);
+	URL.revokeObjectURL(url);
+
+	var link = document.createElement('a');
+	link.download = new Date().toISOString() + '.'+encoding;
+	link.innerHTML = link.download;
+
+	var blob = new Blob([url], {type: "audio/wav"});
+	var fileName = link.download;
+	// saveAs(blob,fileName);
+	file = window.localStorage.getItem(fileName, blob);
+	reader.readAsDataURL(blob);
+}
